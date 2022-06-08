30a147da3d3d5f7738696a2845cd4c7182af8987
spinatose <spinatose@gmail.com>
2022-06-08 10:25:57 -0500

    repo init and create

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System.Threading.Tasks;
+using Microsoft.Identity.Client;
+
+public class Program 
+{
+    private const string _clientId = "7c523e0a-fd3a-46dd-a561-b33c0d28f98a";
+    private const string _tenantId = "efef44b6-5f95-4ffa-b80b-1715dfc2d086";
+
+    static async Task Main(string[] args) 
+    {
+        // See https://aka.ms/new-console-template for more information
+        Console.WriteLine("Hello, World!");
+
+        var app = PublicClientApplicationBuilder
+            .Create(_clientId)
+            .WithAuthority(AzureCloudInstance.AzurePublic, _tenantId)
+            .WithRedirectUri("http://localhost")
+            .Build();
+
+        string[] scopes = { "user.read" };
+
+        AuthenticationResult result = await app.AcquireTokenInteractive(scopes).ExecuteAsync();
+        System.Console.WriteLine($"Token: \t{ result.AccessToken }");
+    }
+}
---
+++ readme.md
@@ -0,0 +1,11 @@
+## Azure Portal
+### Register app in Azure Active Directory service as new app registration
+| Field   | Value  |
+|---|---|
+|Name | az204appreg|
+|Supported account types | Select Accounts in this organizational directory only|
+|Redirect URI (optional) | Select Public client/native (mobile & desktop) and enter http://localhost in the box to the right.|
+
+
+- Get nuget package for MSAL
+`dotnet add package Microsoft.Identity.Client`
